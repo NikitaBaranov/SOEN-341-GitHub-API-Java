162172b4d02d739753253b56ff0838b16f2d3c08
Nikita Baranov <NikitaBaranov@gmail.com>
2017-02-10 03:46:00 +0500

    Changer to run Travis

---
+++ src/main/java/ca/concordia/SOEN_341/W17/G4/App.java
@@ -0,0 +1,13 @@
+package ca.concordia.SOEN_341.W17.G4;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
