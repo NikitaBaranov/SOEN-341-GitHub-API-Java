5b1f94600d9ee4c3c5c892c9367ba3c49b5f434d
Nikita Baranov <NikitaBaranov@gmail.com>
2017-02-10 03:42:09 +0500

    Unit test to run

---
+++ src/main/java/ca/concordia/SOEN_341/W17/G4/TestTravisCI.java
@@ -0,0 +1,17 @@
+package ca.concordia.SOEN_341.W17.G4;
+
+public class TestTravisCI {
+    static String sumStr(String a, String b) {
+        return new StringBuilder().append(a).append(b).toString();
+    }
+
+    static String sumStrWithSpace(String a, String b) {
+        return new StringBuilder().append(a).append(" ").append(b).toString();
+    }
+
+    public static void main(String args[]) {
+        System.out.print(sumStrWithSpace("Hello,","World!"));
+        //System.out.print(sumStr("Hello","World!"));
+    }
+}
+
---
+++ src/test/java/ca/concordia/SOEN_341/W17/G4/TestTravisCITest.java
@@ -0,0 +1,17 @@
+package ca.concordia.SOEN_341.W17.G4;
+
+import junit.framework.TestCase;
+
+public class TestTravisCITest extends TestCase {
+    public void testSumStr() throws Exception {
+        assertEquals("AB",TestTravisCI.sumStr("A","B"));
+        assertEquals("BA",TestTravisCI.sumStr("B","A"));
+    }
+
+    public void testSumStrWithSpace() throws Exception {
+        assertEquals("A B",TestTravisCI.sumStrWithSpace("A","B"));
+        assertEquals("B A",TestTravisCI.sumStrWithSpace("B","A"));
+
+    }
+
+}
